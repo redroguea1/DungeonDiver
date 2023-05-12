2244b5526246652cfa102e9f5e7a978466df980b
redroguea1 <macdickerson@gmail.com>
2023-05-11 22:00:14 -0700

    styling underway stage 1 of many

--- main_app/views.py
+++ main_app/views.py
@@ -28,24 +28,6 @@ def divers_detail(request, diver_id):
       'item_form': item_form
       })
 
-def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
 
 #need to require login
 class DiverCreate(CreateView):
@@ -87,3 +69,24 @@ def search_items(request, diver_id):
       'diver': diver,
       'item': charItem
    })
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      #ADDED THE USER SAVE  
+      user.save()
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
